a9a00ad83fdef0ef3ddbbab3ff7106e1cd9eb854
NOHI <thisisnohi@163.com>
2024-06-26 17:33:19 +0800

    fix 修改代码规约问题

--- src/main/java/nohi/utils/BasicValueFieldComparator.java
+++ src/main/java/nohi/utils/BasicValueFieldComparator.java
@@ -3,13 +3,17 @@ package nohi.utils;
 import java.lang.reflect.Field;
 import java.util.Comparator;
 
+/**
+ * 对象比较
+ * @author nohi
+ */
 public class BasicValueFieldComparator implements Comparator<Field>{
 
 	@Override
 	public int compare(Field f1, Field f2) {
-		if (null == f1.getName() || "".equals(f1.getName().trim())) {
+		if (f1.getName().trim().isEmpty()) {
 			return -1;
-		}else if (null == f2.getName() || "".equals(f2.getName().trim())) {
+		}else if (f2.getName().trim().isEmpty()) {
 			return 1;
 		}else {
 			return f1.getName().compareTo(f2.getName());
